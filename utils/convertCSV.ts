cd76dc8b9d59396dfe320a5446dfbe0ee57aeee8
BsmKdd <BsmKdd@outlook.com>
2023-09-12 09:47:43 +0300

    initial work with chartJS and data processing

--- utils/convertCSV.ts
+++ utils/convertCSV.ts
@@ -1,25 +1,28 @@
-import * as fs from 'fs';
-import { parse } from 'csv-parse';
+import * as fs from 'fs'
+import { parse } from 'csv-parse'
 
 export const convertToJSON = async (csvFilePath: string): Promise<any> => {
-    let records: object[] = [];
+    let records: object[] = []
     const parser = parse({
         delimiter: ',',
-        from_line: 2,
+        from_line: 3016,
         relax_quotes: true,
-        columns: ['Date', 'Players', 'Average Players', 'Twitch Viewers'],
-    });
-    fs.createReadStream(csvFilePath).pipe(parser);
+        cast: true,
+        trim: true,
+        columns: ['date', 'players', 'averagePlayers', 'twitchViewers'],
+    })
+    fs.createReadStream(csvFilePath).pipe(parser)
 
     for await (const row of parser) {
-        records.push(row);
+        records.push(row)
+        console.log(row)
     }
 
-    const JSONContent = JSON.stringify(records);
+    const JSONContent = JSON.stringify(records)
 
     fs.writeFile('./data/chart.json', JSONContent, (err) => {
-        if (err) return console.log(err);
+        if (err) return console.log(err)
 
-        console.log(`${csvFilePath} was copied as a JSON.`);
-    });
-};
+        console.log(`${csvFilePath} was copied as a JSON.`)
+    })
+}
